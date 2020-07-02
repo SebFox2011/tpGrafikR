5e0fd4e653ecc5b3edbc8eec744afc124a7ec05b
Seb <sebastien.philippe5@gmail.com>
2020-07-02 21:13:30 +0200

    Initialisation du TP recettes grafikArt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import { LoginForm } from './app/LoginForm';
 
-function App() {
+export default function App() {
+  const [user, setUser] = useState(null)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    user ?<div >Bonjour tout le monde</div> : <LoginForm onConnect={setUser}/>
+  )
 }
-
-export default App;
---
+++ src/app/LoginForm.jsx
@@ -0,0 +1,56 @@
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
+
+export function LoginForm() {
+
+    const [error, setError] = useState(null)
+    const [isLoading, setLoading] = useState(false)
+
+    const  handleSubmit = async (event) => {
+        setError(null)
+        setLoading(true)
+        event.preventDefault()
+        console.log(event.target)
+        console.log(event.target[0].value)
+        console.log(event.target[1].value)
+        const data = new FormData(event.target)
+        const response = await fetch('http://localhost:3333/login',{
+            method:'POST',
+            body:data,
+            credentials:'include',
+            headers:{
+                Accept: 'application/json'
+            }
+        })
+        const responseData = await response.json()
+        if (response.ok) {
+
+        }
+        else {
+            setError(responseData.errors[0].message)
+        }
+        setLoading(false)
+    }
+
+    return <form className='container mt-4' onSubmit={handleSubmit}>
+        <h2>Se connecter</h2>
+        {error && <Alert>{error}</Alert>}
+        <div className="group-form">
+            <label htmlFor="username">Nom d'utilisateur</label>
+            <input name="email" type="text" id='username' className='form-control' required/>
+        </div>
+        <div className="group-form">
+            <label htmlFor="password">Mot de passe</label>
+            <input name="password" type="password" id='password' className='form-control' required/>
+        </div>
+        <button disabled={isLoading} type='submit' className='btn btn-primary'>Se connecter</button>
+    </form>
+}
+LoginForm.propTypes = {
+    onConnect: PropTypes.func.isRequired
+}
+
+function Alert ({children}) {
+
+    return <div className='alert alert-danger'>{children}</div>
+}
